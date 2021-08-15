0cd8dbff7cc4e506039c30c3a2dd9ef9dc7e0a1c
Tal Feiner <feinertal@gmail.com>
2021-08-15 19:13:58 +0300

    update

--- mat/imu_filter_madgwick_subscriber.m
+++ mat/imu_filter_madgwick_subscriber.m
@@ -4,12 +4,15 @@ main();
 
 
 function imu_cb(imu_sub, imu_msg)
-    showdetails(imu_msg);
+    global imu
+%     showdetails(imu_msg);
+    imu = imu_msg;
 end
 
 
 
 function main()
+    global imu
     if ~robotics.ros.internal.Global.isNodeActive
         rosinit;
     end
@@ -17,13 +20,41 @@ function main()
     imu_pub = robotics.ros.Publisher(imu_sub_node, '/imu/madgwick', 'sensor_msgs/Imu');
     imu_sub = robotics.ros.Subscriber(imu_sub_node, '/imu/madgwick', 'sensor_msgs/Imu', @imu_cb);
 %     imu = receive(imu_sub);
-    r = robotics.ros.Rate(imu_sub_node, 1);  % Hz
-    while robotics.ros.internal.Global.isNodeActive
+    r = robotics.ros.Rate(imu_sub_node, 0.5);  % Hz
+%     while robotics.ros.internal.Global.isNodeActive
+    for ii = 1:50
         waitfor(r);
+%         disp(imu.Header.Seq);
+        angular_vel = [imu.AngularVelocity.Y, -imu.AngularVelocity.X, imu.AngularVelocity.Z];
+        accel = [imu.LinearAcceleration.Y, -imu.LinearAcceleration.X, imu.LinearAcceleration.Z];
+        quat = [imu.Orientation.W imu.Orientation.X imu.Orientation.Y imu.Orientation.Z];
+        eulXYZ = quat2eul(quat,'XYZ');
+        roll = eulXYZ(2);
+        pitch = -eulXYZ(1);
+        yaw = eulXYZ(3);
+        euler.roll{ii} = roll;
+        euler.pitch{ii} = pitch;
+        euler.yaw{ii} = yaw;
+%         orient_cov = imu.OrientationCovariance;
+        disp(euler.yaw{ii});
     end
+    save('yaw.mat', '-struct', 'euler')
 end
-
 
 
 % rosshutdown
 % clear('imu_sub_node')
+
+
+%%
+yaw_mat = cell2mat(yaw)*180/pi;
+pitch_mat = cell2mat(pitch)*180/pi;
+roll_mat = cell2mat(roll)*180/pi;
+
+figure('Name','Around roll');
+plot(roll_mat)
+
+hold on
+plot(pitch_mat)
+plot(yaw_mat)
+legend('roll','pitsh', 'yaw')
---
+++ mat/imu_vel.m
@@ -0,0 +1,53 @@
+import robotics.*
+
+main();
+
+
+function imu_cb(imu_sub, imu_msg)
+    global imu
+%     showdetails(imu_msg);
+    imu = imu_msg;
+end
+
+
+function main()
+    global imu
+    if ~robotics.ros.internal.Global.isNodeActive 
+        rosinit;
+    end
+    imu_sub_node = robotics.ros.Node('/imu_sub_node');
+    imu_pub = robotics.ros.Publisher(imu_sub_node, '/imu/madgwick', 'sensor_msgs/Imu');
+    imu_sub = robotics.ros.Subscriber(imu_sub_node, '/imu/madgwick', 'sensor_msgs/Imu', @imu_cb);
+%     imu = receive(imu_sub);
+    linear_vel = 0;
+    linear_position = 0;
+    angular_position = 0;
+    old_imu = 0;
+    rate = 10;
+    dt = 0;
+    r = robotics.ros.Rate(imu_sub_node, rate);  % Hz
+    while robotics.ros.internal.Global.isNodeActive
+%         disp(imu.Header.Seq);
+        angular_vel = [imu.AngularVelocity.Y, -imu.AngularVelocity.X, imu.AngularVelocity.Z];
+        linear_accel = [imu.LinearAcceleration.Y, -imu.LinearAcceleration.X, imu.LinearAcceleration.Z];
+        linear_accel = (linear_accel/norm(linear_accel))*(norm(linear_accel)-9.8);
+        if old_imu ~= 0
+            dt = (imu.Header.Stamp.Nsec - old_imu.Header.Stamp.Nsec)*1e-9;
+%             disp('dt : ')
+%             disp(dt)
+        end
+        old_imu = imu;
+        linear_vel = linear_accel*dt + linear_vel;
+        linear_position = linear_vel*dt + linear_position;
+        angular_position = angular_vel*dt + angular_position;
+%         disp('linear_vel : ')
+%         disp(linear_vel)
+%         disp('angular_vel: ')
+%         disp(angular_vel)
+        waitfor(r);
+    end
+end
+
+
+% rosshutdown
+% clear('imu_sub_node')
